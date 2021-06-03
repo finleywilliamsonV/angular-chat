0b0c1195581257cb9f31a22dbe8d3e0b9c7542b1
Finley Williamson V <finley.williamson@gmail.com>
2021-06-03 13:15:35 -0400

    Add auth service

--- src/app/login-modal/login-modal.component.ts
+++ src/app/login-modal/login-modal.component.ts
@@ -1,8 +1,10 @@
+import { AuthService } from './../shared/services/auth.service';
 import { User } from './../shared/objects/user';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Component, Input, OnInit } from '@angular/core';
 import { ModalBindings } from 'app/shared/models/modal.model';
 import { NgModel } from '@angular/forms';
+import { Router } from '@angular/router';
 
 /**
  * Property Bindings for the Login Modal
@@ -26,15 +28,19 @@ export class LoginModalComponent implements OnInit {
 
     // member variables
     public attemptingLogin: boolean
+    public passwordIncorrect: boolean
 
     /**
      * Constructor
      * @param modalInstance 
      */
     constructor(
-        public modalInstance: NgbActiveModal
+        private modalInstance: NgbActiveModal,
+        private authService: AuthService,
+        private router: Router
     ) {
-        this. attemptingLogin = false
+        this.attemptingLogin = false
+        this.passwordIncorrect = false
     }
 
     /**
@@ -52,8 +58,24 @@ export class LoginModalComponent implements OnInit {
 
     /**
      * Attempts to log the user in with the given password
+     * @param passwordInput 
+     * @returns Promise
      */
-    public attemptLogin(passwordInput: NgModel): void {
+    public async attemptLogin(passwordInput: NgModel): Promise<void> {
+        
+        // store the password from the input
         const password: string = passwordInput.value
+
+        // attempt to log in the user
+        this.attemptingLogin = true
+        const loginSuccessful: boolean = await this.authService.authorizeUser(this.user, password)
+        this.attemptingLogin = false
+
+        // handle success/failure
+        if (loginSuccessful) {
+            this.modalInstance.close()
+        } else {
+            this.passwordIncorrect = true
+        }
     }
 }
---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,56 @@
+import { User } from './../objects/user';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+
+    // member vars
+    private _userAuthorized: boolean
+    private _authorizedUser: User
+
+    /**
+     * Constructor
+     */
+    constructor() {
+        this._userAuthorized = false
+    }
+
+    /**
+     * Attempts to authorize a user
+     * @param user 
+     * @param password 
+     * @returns 
+     */
+    public async authorizeUser(user: User, password: string): Promise<boolean> {
+
+        // await mock server call
+        await this.mockServerCall(1000)
+
+        // check the password 
+        if (user.password === password) {
+
+            // set as authorized user
+            this._userAuthorized = true
+            this._authorizedUser = user
+            return true
+        }
+        return false
+    }
+
+    /**
+     * Creates a mock server call taking a given amount of time
+     * @param timeoutLength time in ms
+     * @returns Promise -> void
+     */
+    private async mockServerCall(timeoutLength: number): Promise<void> {
+        return new Promise<void>(
+            (resolve) => {
+                setTimeout(() => {
+                    resolve()
+                }, timeoutLength)
+            }
+        )
+    }
+}
